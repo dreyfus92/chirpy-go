d47079de9b3f82e6d9e2a87236a535f05c9e99a8
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2023-07-12 18:11:59 -0600

    add: added stateful handler that checks number of hits to endpoint

--- main.go
+++ main.go
@@ -1,23 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handlerMetrics(cfg *apiConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+	}
+}
 
 func main(){
 	filepathRoot := "./"
 	port := "8080"
+	cfg := &apiConfig{}
 
 	//initializing mux
 	mux := http.NewServeMux()
 
-	// adding handlers
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	// adding app handler
+	mux.Handle("/app/", cfg.middlewareMetricsInc((http.StripPrefix("/app", http.FileServer(http.Dir("."))))))
+	//adding healthz handler
 	mux.HandleFunc("/healthz", handlerReadiness)
-
+	//adding metrics handler
+	mux.HandleFunc("/metrics", handlerMetrics(cfg))
 	//applying middleware
 	corsMux := middlewareCors(mux)
 
@@ -38,3 +59,4 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request){
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
+
