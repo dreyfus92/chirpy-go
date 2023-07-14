b1d17fcd20c0466c75a95755bcd9c6ef4c52e4c6
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2023-07-14 17:43:17 -0600

    feat: implemented helpers to dry code and clean func

--- validate_chirp.go
+++ validate_chirp.go
@@ -2,33 +2,63 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"strings"
 )
 
 type paramsValidateChirp struct {
 	Body string `json:"body"`
 }
 
+type returnVals struct{
+	CleanedBody string `json:"cleaned_body"`
+}
+
+var profanities = []string{
+	"kerfuffle",
+	"sharbert",
+	"fornax",
+}
+
+
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request){
 	w.Header().Add("Content-Type", "application/json")
 
 	decoder := json.NewDecoder(r.Body)
 	chirp := paramsValidateChirp{}
-
 	err := decoder.Decode(&chirp)
+
+	const maxChirpLength = 140
+	const minChirpLength = 1
+
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(`{"error": "Something went wrong"}`))
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
 		return
 	}
 
-	if len(chirp.Body) > 140 {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"error": "Chirp is too long"}`))
+	if len(chirp.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest,"Chirp is too long")
 		return
 	}
 
+	cleaned_body := cleanProfanities(chirp.Body)
 
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"valid": true}`))
+	respondWithJSON(w, http.StatusOK, returnVals{
+		CleanedBody: cleaned_body,
+	})
 }
+
+func cleanProfanities(body string) string {
+	bodyWords := strings.Split(body, " ")
+	log.Printf("Body words: %v", bodyWords)
+	for i, word := range bodyWords {
+		for _, profanity := range profanities {
+			if strings.Contains(strings.ToLower(word), profanity) {
+				bodyWords[i] = strings.Repeat("*", 4)
+			}
+		}
+	}
+	return strings.Join(bodyWords, " ")
+}
+
